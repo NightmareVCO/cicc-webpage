cf4aa85e5d36c599dd47c6777f35e7a87ba8ef26
NightmareVCO <vladimircuriel@outlook.com>
2024-12-15 02:44:03 -0400

    feat(tailwind): Enhance Tailwind configuration with new colors, background images, and flex utilities

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,7 +9,20 @@ export default {
 				'primary': '#00589B',
 				'secondary': '#E9A900',
 				'accent': '#004376',
+				'accent-dark': '#001B30',
+				},
+			backgroundImage: {
+				'hero-image': "url('/hero.webp')",
+				'hero-gradient': "linear-gradient(110deg, #00589B 20%, #001B3060 100%)",
 			},
+			flex: {
+				'1': '1 1 0%',
+				'2': '2 2 0%',
+				'3': '3 3 0%',
+				'4': '4 4 0%',
+				'5': '5 5 0%',
+			},
+
 		},
 	},
 	plugins: [],
