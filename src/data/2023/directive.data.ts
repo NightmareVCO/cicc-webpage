5870e2c5ce11e5f1af254547a6a0b770d810628d
NightmareVCO <vladimircuriel@outlook.com>
2024-12-17 01:05:23 -0400

    fix(directive): Update LinkedIn URLs for committee members and adjust image paths in index.astro

--- src/data/2023/directive.data.ts
+++ src/data/2023/directive.data.ts
@@ -7,7 +7,7 @@ export const directive_2023: Directive[] = [
     description:
       "El presidente del Comité es el encargado de liderar la asociación y velar por el cumplimiento de los estatutos y la buena marcha de la asociación.",
     image: "/directive/2023/Darvy.webp",
-    href: "https://www.linkedin.com/in/darvy-betances-9b1a0b1b4/",
+    href: "https://www.linkedin.com/in/darvybm/",
   },
   {
     name: "Anthony Beato",
@@ -15,7 +15,7 @@ export const directive_2023: Directive[] = [
     description:
       "El vicepresidente del Comité es el encargado de apoyar al presidente en sus funciones y de suplirlo en caso de ausencia.",
     image: "/directive/2023/Anthony.webp",
-    href: "https://www.linkedin.com/in/anthony-beato-7b6b0a1b4",
+    href: "https://www.linkedin.com/in/anthony-beato-abreu-2b9a3a219?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
   },
   {
     name: "Marcos Blanco",
@@ -23,7 +23,7 @@ export const directive_2023: Directive[] = [
     description:
       "La secretaria del Comité es la encargada de llevar las actas de las reuniones y de mantener la comunicación con los miembros de la asociación.",
     image: "/directive/2023/Marcos.webp",
-    href: "https://www.linkedin.com/in/marcos-blanco-7b6b0a1b4",
+    href: "https://www.linkedin.com/in/marcos-white?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
   },
   {
     name: "Albert García",
@@ -31,7 +31,6 @@ export const directive_2023: Directive[] = [
     description:
       "La encargada de logística y eventos es la responsable de organizar las actividades y eventos de la asociación, así como de coordinar con los demás miembros del Comité.",
     image: "/directive/2023/Albert.webp",
-    href: "https://www.linkedin.com/in/albert-garcia-7b6b0a1b4",
   },
   {
     name: "Carlos Peterson",
@@ -39,7 +38,7 @@ export const directive_2023: Directive[] = [
     description:
       "El fotógrafo del Comité es el encargado de capturar los momentos más importantes de la asociación y de documentar las actividades de la asociación.",
     image: "/directive/2023/Carlos.webp",
-    href: "https://www.linkedin.com/in/carlos-peterson-7b6b0a1b4",
+    href: "https://www.linkedin.com/in/carlos-eleazar-peterson-garc%C3%ADa-62306626a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
   },
   {
     name: "Gabriel Cepeda",
@@ -47,7 +46,7 @@ export const directive_2023: Directive[] = [
     description:
       "El tesorero del Comité es el encargado de llevar las finanzas de la asociación y de velar por el buen uso de los recursos económicos.",
     image: "/directive/2023/Gabriel.webp",
-    href: "https://www.linkedin.com/in/gabriel-cepeda-7b6b0a1b4",
+    href: "https://www.linkedin.com/in/gabriel-cepeda-023983270",
   },
   {
     name: "Bryant Reynoso",
@@ -55,7 +54,6 @@ export const directive_2023: Directive[] = [
     description:
       "El web master del Comité es el encargado de mantener y actualizar la página web de la asociación, así como de coordinar con los demás miembros del Comité.",
     image: "/directive/2023/Bryant.webp",
-    href: "https://www.linkedin.com/in/bryant-reynoso-7b6b0a1b4",
   },
   {
     name: "Eduardo Martínez",
@@ -63,7 +61,7 @@ export const directive_2023: Directive[] = [
     description:
       "El relacionador público del Comité es el encargado de mantener la comunicación con los miembros de la asociación y de promover las actividades de la asociación en la comunidad.",
     image: "/directive/2023/Eduardo.webp",
-    href: "https://www.linkedin.com/in/eduardo-martinez-7b6b0a1b4",
+    href: "https://www.linkedin.com/in/eduardo-mart%C3%ADnez-2b4527278",
   },
 ];
 
