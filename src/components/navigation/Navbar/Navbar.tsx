79463d509ffdd68e48b31eb284dccc56ada9a58d
NightmareVCO <vladimircuriel@outlook.com>
2024-12-19 02:01:01 -0400

    fix(navbar): adjust layout for better alignment of items in Navbar component

--- src/components/navigation/Navbar/Navbar.tsx
+++ src/components/navigation/Navbar/Navbar.tsx
@@ -13,7 +13,7 @@ export default function Navbar() {
       id="navbar"
       className="fixed top-0 z-50 w-dvw bg-accent md:bg-accent/30"
     >
-      <div className="flex flex-wrap items-center justify-center max-w-screen-xl p-6 mx-auto">
+      <div className="flex flex-wrap items-center justify-between max-w-screen-xl p-6 mx-auto md:justify-center">
         <a href="/#inicio" className="md:hidden">
             <img
               src="/favicon.webp"
